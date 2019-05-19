d6841c3958794b97a34ebc09dd9212b80c385b91
Frostphyr <omoshulol@gmail.com>
2019-05-19 14:51:37 -0400

    Added workaround for Jekyll not applying pretty permalink to PHP files

--- _layouts/latest-download.php
+++ _layouts/latest-download.php
@@ -2,14 +2,5 @@
 ---
 <?php
 
-{% assign releases = site.pages | where: "layout", "release" | where: "project-id", page.project-id | sort: "date" %}
-{% assign latest_release = 0 %}
-{% for release in releases %}
-    {% assign major = release.version | split: "." | first | plus: 0 %}
-    {% if latest_release == 0 or major >= highest_major %}
-        {% assign latest_release = release %}
-        {% assign highest_major = major %}
-    {% endif %}
-{% endfor %}
-
-include($_SERVER['DOCUMENT_ROOT'] . "/{{ page.projects[page.project-id].url }}/releases/download/{{ latest_release.version }}.php");
+{% include latest-release.html %}
+include("{{ latest_release.version }}/index.php");
